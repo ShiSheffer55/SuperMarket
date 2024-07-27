103e5b56ada29d25fb1ac0e8a18b762c8bd8b00b
ShiSheffer55 <shirasheffer839@gmail.com>
2024-07-26 17:13:35 -0700

    loginUser and registerUser in the usersController

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { isAdmin } = require('../controllers/passController');
 const {
-    getAllProducts,
-    renderAddProductForm,
-    addProduct,
-    renderEditProductForm,
-    updateProduct,
-    deleteProduct,
     getRecommendedProducts,
     getProductsFromCollection
 } = require('../controllers/productsController');
@@ -18,15 +11,5 @@ router.get('/', getRecommendedProducts);
 // Route to get products from a specific collection
 router.get('/:collectionName', getProductsFromCollection);
 
-// Route to add a product (admin only)
-router.get('/add', isAdmin, renderAddProductForm);
-router.post('/add', isAdmin, addProduct);
-
-// Route to edit a product (admin only)
-router.get('/edit/:id', isAdmin, renderEditProductForm);
-router.post('/edit/:id', isAdmin, updateProduct);
-
-// Route to delete a product (admin only)
-router.post('/delete/:id', isAdmin, deleteProduct);
 
 module.exports = router;
