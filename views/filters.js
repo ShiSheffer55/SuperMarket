11aeb97bf6a9fc19933d84c908cba36c5243c4af
AdvaReuveni <advushki@gmail.com>
2024-07-27 16:13:04 +0300

    code for product filters java script+design

---
+++ views/filters.js
@@ -0,0 +1,24 @@
+
+// function ShowBar()
+// {
+//     document.getElementsByClassName('ul2')[0].style.visibility = 'visible';
+// }
+
+// function HideBar()
+// {
+//     document.getElementsByClassName('ul2')[0].style.visibility='hidden';
+// }
+
+
+
+function toggleFilters() {
+    const filterBar = document.getElementById('filterBar');
+    filterBar.style.display = filterBar.style.display === 'none' ? 'block' : 'none';
+}
+
+function applyFilters(){}
+
+
+
+
+
