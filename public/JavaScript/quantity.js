1fa3c65076c3deb384cac6c7278d63b182c38d97
romizeltser <166422905+romizeltser@users.noreply.github.com>
2024-07-29 01:20:53 +0300

    updated quantity func but need to change

--- public/JavaScript/quantity.js
+++ public/JavaScript/quantity.js
@@ -1,9 +1,10 @@
-
-    function changeQuantity(delta) {
-        const input = document.getElementById('quantity');
-        let value = parseInt(input.value, 10) || 0;
-        value += delta;
-        if (value < parseInt(input.min, 10)) value = input.min;
-        if (value > parseInt(input.max, 10)) value = input.max;
-        input.value = value;
-    }
+function changeQuantity(button, delta) {
+    console.log('Button clicked:', button, 'Delta:', delta); // בדיקת לחיצה על כפתור
+   // const input = button.parentElement.querySelector('.quantity-input');
+    let value = parseInt(input.value, 10) || 0;
+    value += delta;
+    if (value < parseInt(input.min, 10)) value = input.min;
+    if (value > parseInt(input.max, 10)) value = input.max;
+    input.value = value;
+    console.log('New quantity value:', input.value); // בדיקת ערך כמות חדשה
+}
