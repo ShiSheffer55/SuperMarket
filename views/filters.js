8942d7e066452c45378edf60c8b7801d94f1a43b
AdvaReuveni <advushki@gmail.com>
2024-07-28 13:11:57 +0300

    I added a slider to filter price by price range

--- views/filters.js
+++ views/filters.js
@@ -1,16 +1,3 @@
-
-// function ShowBar()
-// {
-//     document.getElementsByClassName('ul2')[0].style.visibility = 'visible';
-// }
-
-// function HideBar()
-// {
-//     document.getElementsByClassName('ul2')[0].style.visibility='hidden';
-// }
-
-
-
 function toggleFilters() { 
     const filterBar = document.getElementById('filterBar');
     filterBar.style.display = filterBar.style.display === 'none' ? 'block' : 'none';
