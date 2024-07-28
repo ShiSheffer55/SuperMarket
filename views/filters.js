cedcdd4ba95bc3f2f8aee9261cd2174104cb5e4f
AdvaReuveni <advushki@gmail.com>
2024-07-28 12:38:16 +0300

    I added a slider to filter price by price range

--- views/filters.js
+++ views/filters.js
@@ -11,14 +11,27 @@
 
 
 
-function toggleFilters() {
+function toggleFilters() { 
     const filterBar = document.getElementById('filterBar');
     filterBar.style.display = filterBar.style.display === 'none' ? 'block' : 'none';
 }
 
-function applyFilters(){}
 
+function applyFilters() {}
 
+$(function() { //פונקציה בשביל טווח המחירים
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [75, 300],
+        slide: function(event, ui) {
+            $("#price-range").text("₪" + ui.values[0] + " - ₪" + ui.values[1]);
+        }
+    });
+    $("#price-range").text("₪" + $("#slider-range").slider("values", 0) +
+        " - ₪" + $("#slider-range").slider("values", 1));
+});
 
 
 
