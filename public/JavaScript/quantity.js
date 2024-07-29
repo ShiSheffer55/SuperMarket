9d428adb272f7195a9d212bcd2a3af6e51709841
noamlugassi1 <noamlugassi1@gmail.com>
2024-07-29 13:42:25 +0300

    fixed quantity

--- public/JavaScript/quantity.js
+++ public/JavaScript/quantity.js
@@ -1,10 +1,12 @@
-function changeQuantity(button, delta) {
-    console.log('Button clicked:', button, 'Delta:', delta); // בדיקת לחיצה על כפתור
-   // const input = button.parentElement.querySelector('.quantity-input');
-    let value = parseInt(input.value, 10) || 0;
-    value += delta;
-    if (value < parseInt(input.min, 10)) value = input.min;
-    if (value > parseInt(input.max, 10)) value = input.max;
-    input.value = value;
-    console.log('New quantity value:', input.value); // בדיקת ערך כמות חדשה
-}
+function changeQuantity(button, increment) {
+    var input = button.parentElement.querySelector('input');
+    var currentValue = parseInt(input.value);
+    var newValue = currentValue + increment;
+    var min = parseInt(input.min);
+    var max = parseInt(input.max);
+  
+    if (newValue >= min && newValue <= max) {
+      input.value = newValue;
+    }
+  }
+  
