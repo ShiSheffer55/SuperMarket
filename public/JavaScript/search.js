c7eb5e99613a57f370d4c7ce75273b1f64c0a8d6
romizeltser <166422905+romizeltser@users.noreply.github.com>
2024-07-30 22:41:11 +0300

    fixed quantity funcs

--- public/JavaScript/search.js
+++ public/JavaScript/search.js
@@ -18,15 +18,3 @@ $( function() {
     });
 });
 
-function changeQuantity(button, delta) {
-    var input = $(button).siblings('.custominput');
-    var newValue = parseInt(input.val()) + delta;
-    var max = parseInt(input.attr('max'));
-    if (newValue >= 1 && newValue <= max) {
-        input.val(newValue);
-    }
-}
-
-function resetQuantity() {
-    $(".custominput").val(1);
-}
