40bbc623922e6e38b3ed05b84fc1f0b2a889c79c
romizeltser <166422905+romizeltser@users.noreply.github.com>
2024-08-01 22:10:46 +0300

    Revert "some changes in the map - not done"
    This reverts commit a48311c6f8056bec8416b80a3460b95a21b3045b.

--- public/JavaScript/deleteProduct.js
+++ public/JavaScript/deleteProduct.js
@@ -7,6 +7,7 @@ function deleteProduct(collectionName, id) {
             console.log("Product deleted successfully:", response);
             alert(response.message);
             location.reload();
+            // כאן אפשר לרענן את העמוד או לעדכן את ה-UI בהתאם לצורך
         },
         error: function (xhr, status, error) {
             console.error("Error deleting product:", xhr, status, error);
