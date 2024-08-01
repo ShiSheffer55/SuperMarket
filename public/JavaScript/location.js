a48311c6f8056bec8416b80a3460b95a21b3045b
romizeltser <166422905+romizeltser@users.noreply.github.com>
2024-08-01 21:45:10 +0300

    some changes in the map - not done

--- public/JavaScript/location.js
+++ public/JavaScript/location.js
@@ -1,26 +1,26 @@
   // Initialize the Google Map
   function initMap() {
-    const map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
-        center: { lat: 31.7683, lng: 35.2137 } // Default center, Jerusalem
-    });
-
+  var options = {
+        zoom: 12,
+        center: { lat: 	31.771959, lng: 35.217018 } // Default center, Jerusalem
+    }
+  const map = new google.maps.Map(document.getElementById('map'), options);
     // Fetch locations from the server
-    fetch('/location/locations')
-        .then(response => response.json())
-        .then(locations => {
-            locations.forEach(location => {
-                const marker = new google.maps.Marker({
-                    position: {
-                        lat: location.coordinates[1],
-                        lng: location.coordinates[0]
-                    },
-                    map: map,
-                    title: location.name
-                });
-            });
-        })
-        .catch(error => console.error('Error fetching locations:', error));
+    // fetch('/location/locations')
+    //     .then(response => response.json())
+    //     .then(locations => {
+    //         locations.forEach(location => {
+    //             const marker = new google.maps.Marker({
+    //                 position: {
+    //                     lat: location.coordinates[1],
+    //                     lng: location.coordinates[0]
+    //                 },
+    //                 map: map,
+    //                 title: location.name
+    //             });
+    //         });
+    //     })
+    //     .catch(error => console.error('Error fetching locations:', error));
 }
 
 // Call initMap when the window loads
