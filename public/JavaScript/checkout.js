6bd4a9e3aff216f6541a045a846b82b08a416394
romizeltser <166422905+romizeltser@users.noreply.github.com>
2024-08-04 02:03:35 +0300

    credit card design

--- public/JavaScript/checkout.js
+++ public/JavaScript/checkout.js
@@ -1,31 +1,31 @@
 
 document.getElementById('checkout-form').addEventListener('submit', function(event)
  {
-    const creditCardNumber = document.getElementById('creditCardNumber');
-    const expirationDate = document.getElementById('expirationDate');
-    const cvv = document.getElementById('cvv');
-    const zipCode = document.getElementById('zipCode');
+    // const creditCardNumber = document.getElementById('creditCardNumber');
+    // const expirationDate = document.getElementById('expirationDate');
+    // const cvv = document.getElementById('cvv');
+    // const zipCode = document.getElementById('zipCode');
 
-    if (!creditCardNumber.checkValidity()) {
-        alert(creditCardNumber.title);
-        event.preventDefault();
-        return;
-    }
-    if (!expirationDate.checkValidity()) {
-        alert(expirationDate.title);
-        event.preventDefault();
-        return;
-    }
-    if (!cvv.checkValidity()) {
-        alert(cvv.title);
-        event.preventDefault();
-        return;
-    }
-    if (!zipCode.checkValidity()) {
-        alert(zipCode.title);
-        event.preventDefault();
-        return;
-    }
+    // if (!creditCardNumber.checkValidity()) {
+    //     alert(creditCardNumber.title);
+    //     event.preventDefault();
+    //     return;
+    // }
+    // if (!expirationDate.checkValidity()) {
+    //     alert(expirationDate.title);
+    //     event.preventDefault();
+    //     return;
+    // }
+    // if (!cvv.checkValidity()) {
+    //     alert(cvv.title);
+    //     event.preventDefault();
+    //     return;
+    // }
+    // if (!zipCode.checkValidity()) {
+    //     alert(zipCode.title);
+    //     event.preventDefault();
+    //     return;
+    // }
 });
 document.getElementById('cancel-cart').addEventListener('click', function() {
     // כאן אתה יכול לשים את הכתובת שאליה יופנה המשתמש לאחר לחיצה על כפתור ביטול
