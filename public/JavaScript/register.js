f6a269751035a7e9b7c22e4e61137011a89c5930
romizeltser <166422905+romizeltser@users.noreply.github.com>
2024-08-07 17:47:31 +0300

    fixed some things

--- public/JavaScript/register.js
+++ public/JavaScript/register.js
@@ -9,6 +9,17 @@ document.getElementById('fName').addEventListener('input', function (e) {
     const sanitizedValue = e.target.value.replace(/[^a-zA-Z\u0590-\u05FF\s]/g, '');
     e.target.value = sanitizedValue;
 });
+document.getElementById('lName').addEventListener('keypress', function (e) {
+    const char = String.fromCharCode(e.which || e.keyCode);
+    if (!char.match(/^[\p{L}\s]$/u)) {
+        e.preventDefault();
+    }
+});
+// for cntrl c to numbers
+document.getElementById('lName').addEventListener('input', function (e) {
+    const sanitizedValue = e.target.value.replace(/[^a-zA-Z\u0590-\u05FF\s]/g, '');
+    e.target.value = sanitizedValue;
+});
 document.getElementById('tel').addEventListener('keypress', function (e) {
     const char = String.fromCharCode(e.which || e.keyCode);
     if (!char.match(/[0-9\s-]/)) {
